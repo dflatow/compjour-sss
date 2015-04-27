53a29200aca1fe4eeabd37e47ff7be4902b74de0
daflatow <daflatow@gmail.com>
2015-04-26 22:41:16 -0700

    The number of listed federal executive agency internet domains

---
+++ src/7.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+data = pd.read_csv('https://inventory.data.gov/dataset/fe9eeb10-2e90-433e-a955-5c679f682502/resource/b626ef1f-9019-41c4-91aa-5ae3f7457328/download/federalexecagncyintntdomains03302015.csv')
+print("The number of listed federal executive agency internet domains:", len(data))
