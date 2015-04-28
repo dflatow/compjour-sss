36404ae8ed4b49dd1de8b0e7f04d744deafb191f
daflatow <daflatow@gmail.com>
2015-04-28 16:46:04 -0700

    remove

--- src/css_select.py
+++
@@ -1,9 +0,0 @@
-import bs4
-import requests
-
-
-response = requests.get('http://developerexcuses.com/')
-soup = bs4.BeautifulSoup(response.text)
-m = soup.select("body > div.wrapper > center > a")[0]
-print(m.text)
-
