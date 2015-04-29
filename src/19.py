33c21f9ee194d384d062eb3209ba27d817c63d2a
daflatow <daflatow@gmail.com>
2015-04-29 16:06:33 -0700

    number preliminary reports on aircraft safety incidents/accidents

--- src/19.py
+++ src/19.py
@@ -1,21 +1,4 @@
-import bs4
-import requests
-import re
-import datetime
+import pandas as pd
 
-def parse_dates(data):
-	dates = [re.findall('\d+', x.text) for x in data]
-	dts = [datetime.date(int(x[0]), int(x[1]), int(x[2])) for x in dates]
-	return dts
-
-def count_fridays(dts):
-
-	return len([1 for x in dts if x.weekday() == 4])
-
-response = requests.get('http://www.justice.gov/feeds/opa/justice-news.xml')
-soup = bs4.BeautifulSoup(response.text)
-data = soup.select("channel pubdate")
-
-print('''In the "Justice News" RSS feed maintained by the Justice Department, the number of items published on a Friday:''', count_fridays(parse_dates(data)))
-
-
+data = pd.read_csv("http://www.asias.faa.gov/pls/apex/f?p=100:93::FLOW_EXCEL_OUTPUT_R2161113456916636_en")
+print("The total number of preliminary reports on aircraft safety incidents/accidents in the last 10 business days:", len(data))
